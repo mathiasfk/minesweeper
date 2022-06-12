c101be55492a0bd7a042a36132934ce4f8416c44
mathiasfk <mathias.kriebel@gmail.com>
2022-06-12 16:05:54 -0300

    Button styles

--- src/components/Controls/index.tsx
+++ src/components/Controls/index.tsx
@@ -1,3 +1,4 @@
+import "./controls.css";
 import { GameState } from "../../types/GameState"
 
 type Props = {
@@ -11,7 +12,11 @@ export const Controls: React.FC<Props> = (props) => {
     <div>
         {props.gameState.win && "Congratulations!"}
         {props.gameState.gameover && "Better luck next time..."}
-        {props.gameState.win && <button onClick={props.onClickNext}>Next game</button>}
-        {props.gameState.gameover && <button onClick={props.onClickRestart}>Restart</button>}
+        <div className="Button-container">
+            {props.gameState.win && <button className="Button next" onClick={props.onClickNext}>Next game</button>}
+        </div>
+        <div className="Button-container">
+            {props.gameState.gameover && <button className="Button restart" onClick={props.onClickRestart}>Restart</button>}
+        </div>
     </div>)
 }
