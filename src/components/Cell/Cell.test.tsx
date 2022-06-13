68f3eb68b5bfd154688e781d47f6d7983d9c9f27
mathiasfk <mathias.kriebel@gmail.com>
2022-06-13 20:42:01 -0300

    Fixes #7

--- src/components/Cell/Cell.test.tsx
+++ src/components/Cell/Cell.test.tsx
@@ -5,7 +5,7 @@ import { CellState } from '../../types/CellStatus';
 describe('When the a Cell is rendered', () => {
 
     test('should appear on the screen', () => {
-        render(<Cell index={0} onClick={() => null} status={CellState.Unknown}/>);
+        render(<Cell index={0} onClick={() => null} onRightClick={() => null} status={CellState.Unknown}/>);
 
         const cell = screen.getByTestId('cell');
         expect(cell).toBeInTheDocument();
@@ -16,9 +16,18 @@ describe('When the a Cell is rendered', () => {
         [CellState.Clear, CellState[CellState.Clear]],
         [CellState.Exploded, CellState[CellState.Exploded]],
     ])('when the state is %p the class should be %p', (state: CellState, className: string) => {
-        const { container } = render(<Cell index={0} onClick={() => null} status={state}/>);
+        const { container } = render(<Cell index={0} onClick={() => null} onRightClick={() => null} status={state}/>);
 
         const cell = container.getElementsByClassName(className);
         expect(cell.length).toBe(1);
+    });
+
+    test.each([
+        0, 1, 3, 4, 5, 6, 7, 8
+    ])('it should have the Flagged class with %p neighboring mines', (mines) => {
+        render(<Cell index={0} onClick={() => null} onRightClick={() => null} status={CellState.Flagged} neighboringMines={mines}/>);
+
+        const cell = screen.getByTestId('cell');
+        expect(cell).toHaveClass('Flagged');
     });
 });
--- src/components/Cell/index.tsx
+++ src/components/Cell/index.tsx
@@ -9,10 +9,20 @@ type Props = {
   neighboringMines?: number,
 }
 
+const getClassName = (status: CellState, neighboringMines?: number) => {
+  let variableClass = '';
+  if (status === CellState.Danger){
+    variableClass = `${CellState[status]}${neighboringMines || ''}`;
+  }else{
+    variableClass = CellState[status];
+  }
+  return `Cell  ${variableClass}`;
+}
+
 export const Cell: React.FC<Props> = props => {
   return (
     <div data-testid="cell" 
-        className={`Cell  ${CellState[props.status]}${props.neighboringMines || ''}`}
+        className={getClassName(props.status, props.neighboringMines)}
         onClick={() => props.onClick(props.index)}
         onContextMenu={(e) => {props.onRightClick(props.index); e.preventDefault();}}>
           <p>{props.neighboringMines}</p>
